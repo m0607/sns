1104810285bf033e934e184900e7b01264838fc2
m0607 <bmseina@gmail.com>
2023-07-27 14:37:03 +0900

    20230725自作

--- app/resources/views/posts/posts.blade.php
+++ app/resources/views/posts/posts.blade.php
@@ -1,1 +1,23 @@
-投稿登録押した後の画面
+<!-- bootstrap-datepickerを読み込む -->
+<link rel="stylesheet" type="text/css" href="../bootstrap-datepicker-1.6.4-dist/css/bootstrap-datepicker.min.css">
+<script type="text/javascript" src="../bootstrap-datepicker-1.6.4-dist/js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript" src="../bootstrap-datepicker-1.6.4-dist/locales/bootstrap-datepicker.ja.min.js"></script>
+
+<div class="container">
+    <div class="row mt-4">
+        <div class="col-2">
+            <label>日付：</label>
+        </div>
+        <div class="col-5">
+
+            <!-- ここにカレンダー表示用のテキストボックスを追加 -->
+            <input type="text" class="form-control" id="date_sample">
+
+        </div>
+    </div>
+</div>
+
+<!-- bootstrap-datepickerのjavascriptコード -->
+<script>
+    $('#date_sample').datepicker();
+</script>
--- app/resources/views/reports/reports.blade.php
+++ app/resources/views/reports/reports.blade.php
@@ -1,16 +1,22 @@
 <html>
     <body>
         <div class="col-4">
-        <h1 class="mb-4">違反報告フォーム</h1>
-        <div class="form-group">
-        <div class="alert alert-danger" v-if="errors.id" v-text="errors.id"></div>
-        <div class="alert alert-danger" v-if="errors.target" v-text="errors.target"></div>
+                <h1 class="my-3 h3">報告</h1>
+                <div class="form-group">
+                <div class="alert alert-danger" v-if="errors.id" v-text="errors.id"></div>
+                <div class="alert alert-danger" v-if="errors.target" v-text="errors.target"></div>
 
-        <div class="form-group">
-            <label>報告内容</label>
-            <textarea rows="7" class="form-control" v-model="params.comment"></textarea>
-            <div class="alert alert-danger" v-if="errors.comment" v-text="errors.comment"></div>
+            <div class="form-group">
+                <label>報告内容</label>
+                <div>
+                    <textarea rows="7" class="form-control" v-model="params.comment"></textarea>
+                    <div class="alert alert-danger" v-if="errors.comment" v-text="errors.comment"></div>
+                </div>
+            
+                <div class="large-12 columns text-center">
+                    <button class="button expanded" type="button">報告</button>
+                </div>
+            </div>
         </div>
-
     </body>
 </html>
